05974d15522793e025da5945d4087622f2e7347e
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-05-21 14:14:45 +1000

    Add updateTask in rest.http and Taskmodel.js

--- api 15-05-2022/src/model/Task.model.js
+++ api 15-05-2022/src/model/Task.model.js
@@ -3,21 +3,25 @@ import TaskSchema from "./Task.schema.js";
 // ======== queries
 
 // insert task
-export const insertTask = taskObj => {
-	return TaskSchema(taskObj).save();
+export const insertTask = (taskObj) => {
+  return TaskSchema(taskObj).save();
 };
 
 // read task
 export const readTasks = () => {
-	return TaskSchema.find();
+  return TaskSchema.find();
 };
 
 // delete single task
-export const deleteTask = _id => {
-	return TaskSchema.findByIdAndDelete(_id);
+export const deleteTask = (_id) => {
+  return TaskSchema.findByIdAndDelete(_id);
 };
 
 //@ids as an array
-export const deleteMultipleTasks = _ids => {
-	return TaskSchema.deleteMany({ _id: { $in: _ids } });
+export const deleteMultipleTasks = (_ids) => {
+  return TaskSchema.deleteMany({ _id: { $in: _ids } });
+};
+
+export const updateTask = (filter, obj) => {
+  return TaskSchema.findOneAndUpdate(filter, obj, { new: true });
 };
